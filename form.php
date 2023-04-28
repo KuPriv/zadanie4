8dd4a9a8f4a66f89ad21aa998fa088c78f8afdba
amomAA <chernovvova2016@yandex.ru>
2023-04-28 16:27:18 +0300

    1

--- form.php
+++ form.php
@@ -61,9 +61,12 @@
             
                         <div class="item-2">
                             <select multiple="multiple" name="abilities[]" id="abilities" <?php if ($errors['abilities']) {print 'class="error"';}?>>
-                                <option value="Immortality" <?php if($errors['abilities'] and in_array("Immortality", $values['abilities'])) {print 'selected'}?>>Бессмертие</option>
-                                <option value="Passing through walls" <?php if($errors['abilities'] and in_array("Passing through walls", $values['abilities'])) {print 'selected'}?>>Прохождение сквозь стены</option>
-                                <option value="Levitation" <?php if($errors['abilities'] and in_array("Levitation", $values['abilities'])) {print 'selected'}?>>Левитация</option>
+                                <option value="Immortality" 
+                                <?php if($errors['abilities'] and in_array("Immortality", $values['abilities'])) {print 'selected';}?>>Бессмертие</option>
+                                <option value="Passing through walls" 
+                                <?php if($errors['abilities'] and in_array("Passing through walls", $values['abilities'])) {print 'selected';}?>>Прохождение сквозь стены</option>
+                                <option value="Levitation" 
+                                <?php if($errors['abilities'] and in_array("Levitation", $values['abilities'])) {print 'selected';}?>>Левитация</option>
                                 </select>
                         </div>
             
